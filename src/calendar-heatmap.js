76dcce2333427fcc6e4e88936a3ddb8ac5026654
Kai Boschung <boschung91@gmail.com>
2019-08-24 15:02:06 +0200

    changed some stuff with pfunz

--- src/calendar-heatmap.js
+++ src/calendar-heatmap.js
@@ -167,7 +167,7 @@ function calendarHeatmap() {
             .html(tooltipHTMLForDate(d))
             .style('left', function () { return Math.floor(i / 7) * SQUARE_LENGTH + 'px'; })
             .style('top', function () {
-              return formatWeekday(d.getDay()) * (SQUARE_LENGTH + SQUARE_PADDING) + MONTH_LABEL_PADDING * 2 + 'px';
+              return formatWeekday(d.getDay()) * (SQUARE_LENGTH + SQUARE_PADDING) + MONTH_LABEL_PADDING * 2 + 10 + 'px';
             });
         })
         .on('mouseout', function (d, i) {
